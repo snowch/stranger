386d22c9e1092a72edadd98b5d90c058cd48860d
Erdinc Kaya <erdincka@Erdincs-MBP.lan>
2019-06-06 20:59:14 +0400

    First commit with Grommet

---
+++ src/Workload.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { Box, Text, MaskedInput, RangeInput, Image } from 'grommet';
+import * as Icons from 'grommet-icons';
+
+export class Workload extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {capacity:0};
+        // this.handleClick = this.handleClick.bind(this);
+    }
+
+    render() {
+      return (
+        <Box 
+          direction="row-responsive"
+          justify="center"
+          align="center"
+          pad="large"
+          margin="small"
+          elevation="medium"
+          background="light-1"
+          gap="medium">
+            <Icons.Apps size="large" />
+            <Text>{this.props.Workload.Name}</Text>
+            {/* <RangeInput
+                value={this.state.capacity}
+                onChange={event => this.setState({capacity: event.target.value})}
+            /> */}
+            <MaskedInput
+              mask={[
+                {
+                regexp: /^3[0-9]$/,
+                },
+                { fixed: 'TB' }
+              ]}
+              value={this.state.capacity}
+              min="0"
+              max="10000"
+              step="10"
+              onChange={(event) => {this.setState({capacity: event.target.value })}}
+              />
+        </Box>
+      );
+    }
+  }
